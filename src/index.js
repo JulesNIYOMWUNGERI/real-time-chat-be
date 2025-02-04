f392c309f1c2f93d5d76206541334d73b37d4160
Jules NIYOMWUNGERI <julesniyomwungeri144@gmail.com>
2025-02-04 20:21:19 +0200

    creating user signup

--- src/index.js
+++ src/index.js
@@ -7,9 +7,11 @@ import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 const app = express();
 
-app.use("/api/auth", authRoutes)
-
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`);
